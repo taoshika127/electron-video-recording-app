ab4ebe0884c51bd5db319b0adce96e04e256c715
Sijia Tao <taoshika127@gmail.com>
2025-02-25 17:38:27 -0800

    in progress of completing the modal for several cases

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,8 @@
 import './Modal.css';
 import { useEffect, useState } from 'react';
+import Btn from '../ui/Btn.jsx';
 
-export default function Modal({purpose, handleModalOver}) {
+export default function Modal({purpose, handleModalOver, handleRetake}) {
     const [count, setCount] = useState(3)
     useEffect(() => {
         if (purpose === 'countdown') {
@@ -16,14 +17,18 @@ export default function Modal({purpose, handleModalOver}) {
                 clearInterval(intervalId)
             }
         }
-
-
-    }, [purpose])
+    }, [purpose, handleModalOver])
     return <div className="modal w-full h-full bg-slate-600 bg-opacity-60 flex justify-center items-center">
         {purpose === 'countdown' ? <div className="count kanit-medium text-9xl">{count}
         </div> : null}
-        {purpose === 'confirm retake' ? <div>
-            retake
+        {purpose === 'confirm retake' ? <div className="confirm-modal flex-col p-10 text-left">
+            <p className="text-2xl font-bold">Heads up! Your recording might be lost!</p>
+            If you retake the video, you will lose the data you have recorded so far. Are you sure you want to continue to retake?
+            <div className="w-full flex justify-between mt-10">
+                <Btn text="Continue" handleClick={handleRetake} color="bg-slate-800"/>
+                <Btn text="Cancel" handleClick={handleModalOver} color="bg-slate-300" />
+            </div>
+
         </div> : null}
     </div>
 
--- src/components/VideoControls.jsx
+++ src/components/VideoControls.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import { useReactMediaRecorder, ReactMediaRecorder } from "react-media-recorder";
+import { ReactMediaRecorder } from "react-media-recorder";
 import Video from './Video.jsx';
 import Webcam from "react-webcam";
 import Btn from '../ui/Btn.jsx';
@@ -32,21 +32,27 @@ const videoConstraints = {
 };
 
 export default function VideoControls () {
-    const [showCountdown, setShowCountdown] = useState(true);
+    const [showCountdown, setShowCountdown] = useState(false);
     const [showConfirmRetake, setShowConfirmRetake] = useState(false);
     return (
         <div className="video-controls w-full">
             <ReactMediaRecorder
             video
-            render={({ status, startRecording, stopRecording, mediaBlobUrl, previewStream }) => (
+            render={({ status, startRecording, stopRecording, pauseRecording, resumeRecording, mediaBlobUrl, previewStream }) => (
                 <div className="h-full">
+                    <div>{status}</div>
                     <div className="controls flex justify-center items-center">
-                        {status === 'idle' ? <Btn icon={<CircleIcon />} handleClick={startRecording} text="Start Recording" color="bg-green-400"/> : null}
-                        {status === 'recording' ?
+                        {status === 'idle' ? <Btn icon={<CircleIcon />} handleClick={() => {
+                            setShowCountdown(true)
+                        }} text="Start Recording" color="bg-green-400"/> : null}
+                        {status === 'recording' || status === 'paused' ?
                         (<div className="flex-col justify-center items-center">
                             <div className="flex justify-center items-center gap-10 mb-5">
                                 <Btn icon={<SquareIcon />} handleClick={stopRecording} text="Stop Recording" color="bg-red-500" />
-                                <Btn icon={<UndoIcon />} text="Retake" color="bg-gray-400" />
+                                <Btn icon={<UndoIcon />} handleClick={() => {
+                                    pauseRecording()
+                                    setShowConfirmRetake(true)
+                                }} text="Retake" color="bg-gray-400" />
                             </div>
                             <div className="flex justify-center items-center">
                                 <Counter />
@@ -64,7 +70,7 @@ export default function VideoControls () {
                         </div> : null}
                     </div>
                     <div className="w-full video-screen flex justify-center items-center">
-                        {status === 'idlee' ? <Webcam
+                        {status === 'idle' ? <Webcam
                                                 audio={false}
                                                 screenshotFormat="image/jpeg"
                                                 width={1080}
@@ -74,15 +80,20 @@ export default function VideoControls () {
                         {status === 'recording' ? <VideoPreview stream={previewStream} /> : null}
                         {status === 'stopped' ? <Video src={mediaBlobUrl} autoPlay={false} controls={true} /> : null}
                     </div>
+                    {showCountdown ? <Modal purpose='countdown' handleModalOver={async () => {
+                        startRecording()
+                        setShowCountdown(false)
+                    }}/> : null}
+                    {showConfirmRetake ? <Modal purpose='confirm retake' handleRetake={() => {
+                        window.location.reload()
+                    }} handleModalOver={async () => {
+                        resumeRecording()
+                        setShowConfirmRetake(false)
+                    }} /> : null}
                 </div>
+
             )}
             />
-            {showCountdown ? <Modal purpose='countdown' handleModalOver={() => {
-                setShowCountdown(false)
-            }}/> : null}
-            {showConfirmRetake ? <Modal purpose='confirm retake' handleModalOver={() => {
-                setShowConfirmRetake(false)
-            }}/> : null}
         </div>
     );
 
